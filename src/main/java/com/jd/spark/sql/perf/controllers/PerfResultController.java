adaa57fa21fdf67fcb14206b54bec0800eb5f832
bit1129 <bit1129@163.com>
2015-08-23 12:28:01 +0800

    Push on 201508231227

--- src/main/java/com/jd/spark/sql/perf/controllers/PerfResultController.java
+++ src/main/java/com/jd/spark/sql/perf/controllers/PerfResultController.java
@@ -4,9 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Created by yuzhitao on 2015/8/23.
- */
+
 @RequestMapping("/perf")
 @Controller
 public class PerfResultController {
